cc0e5121e1e084ee19e0ab3dba06245a689a028a
Udipta Pathak <udiptapathak13@gmail.com>
2023-04-17 22:02:30 +0530

    fix: copying files to /usr/include

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
+sudo mkdir -p /usr/include/strip
+sudo cp -rf include/* /usr/include/strip
 yacc -d src/translate.y
 lex src/lex.l
 gcc lex.yy.c y.tab.c -o strip
-sudo mv ./strip /usr/bin
+sudo mv strip /usr/bin
