f3254e250d52ef5974424bd00f621ceec1e06056
Udipta Pathak <udiptapathak13@gmail.com>
2023-04-18 00:45:03 +0530

    feat: create static library and link it for compilation

--- build.sh
+++ build.sh
@@ -2,5 +2,10 @@ sudo mkdir -p /usr/include/strip
 sudo cp -rf include/* /usr/include/strip
 yacc -d src/translate.y
 lex src/lex.l
-gcc lex.yy.c y.tab.c -o strip
+mkdir -p tmp
+cd tmp
+gcc -c ../src/*.c
+ar -rcs libstrip.a *.o
+cd ../
+gcc lex.yy.c y.tab.c -o strip -Ltmp -lstrip
 sudo mv strip /usr/bin
